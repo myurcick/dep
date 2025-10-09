bde0145910c4eca1a0dda4478b4fb4c136a77187
myurcick <166910242+myurcick@users.noreply.github.com>
2025-10-09 22:58:06 +0300

    Update DbInitializer.cs

--- backend/Backend/Data/DbInitializer.cs
+++ backend/Backend/Data/DbInitializer.cs
@@ -7,17 +7,67 @@ namespace ProfkomBackend.Data
     {
         public static void Seed(AppDbContext db)
         {
-            if (!db.Admins.Any()) //додаємо адміна якщо таблиця пуста
+            var adminsToAdd = new List<Admin>
             {
-                var hash = BCrypt.Net.BCrypt.HashPassword("@Admin123");
-                db.Admins.Add(new Admin
+                new Admin
                 {
                     Username = "admin@gmail.com",
-                    PasswordHash = hash,
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("@Admin123"),
                     Role = "admin"
-                });
-                db.SaveChanges();
+                },
+                new Admin
+                {
+                    Username = "Duma@profkom.com",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("@Duma2004"),
+                    Role = "admin"
+                },
+                new Admin
+                {
+                    Username = "Oleksandra.ssf@profkom.com",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("!Slyvka2006"),
+                    Role = "admin"
+                },
+                new Admin
+                {
+                    Username = "sofia@profkom.com",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("!Tereshkevych1"),
+                    Role = "admin"
+                },
+                new Admin
+                {
+                    Username = "Nelia@profkom.com",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("@Ofredr14"),
+                    Role = "admin"
+                },
+                new Admin
+                {
+                    Username = "marusia@profkom.com",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("@InmovToLove"),
+                    Role = "admin"
+                },
+                new Admin
+                {
+                    Username = "bohuslav@profkom.com",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("!Ateist1488"),
+                    Role = "admin"
+                },
+                new Admin
+                {
+                    Username = "Diana@profkom.com",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("!Ekonomist19"),
+                    Role = "admin"
+                }
+            };
+
+            foreach (var admin in adminsToAdd)
+            {
+                if (!db.Admins.Any(a => a.Username == admin.Username))
+                {
+                    db.Admins.Add(admin);
+                }
             }
+
+            db.SaveChanges();
         }
     }
 }
